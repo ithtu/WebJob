0e970bde578ac30b4f1542fff1e7479e9c9aca33
quoctupdn@gmail.com <quoctupdn@gmail.com>
2022-05-07 17:47:01 +0700

    fe-employ

---
+++ WebJob/Controllers/JobsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebJob.Controllers
+{
+    public class JobsController : Controller
+    {
+        // GET: Jobs
+        public ActionResult ListBySkill()
+        {
+            return View();
+        }
+        public ActionResult Details()
+        {
+            return View();
+        }
+    }
+}
